ad04736c3df2d260c8acb5249ab692044c22184f
Elle H <itsellej@gmail.com>
2018-08-19 18:34:42 +0100

    Fix: main function. Colour changer now works

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-// isolate the input field
+const hexInput = document.querySelector('input[type="text"]');
+const submitColour = document.getElementById('buttoncolour');
 
-const hexInput = document.querySelector('#hexchanger > input');
-const docBody = document.querySelector('body');
-
-// add a listener to it
-
-const changeBackground = () => {
-  const hexValue = hexInput.value;
-  console.log('hello');
-  docBody.setAttribute('style', 'background-color: hexValue');
-};
 
 const main = () => {
-  hexInput.addEventListener('input', changeBackground());
-};
+  const changeBackground = () => {
+    const hexValue = hexInput.value;
+    document.body.style.backgroundColor = hexValue;
+  };
 
-// set attribute of body background to field input
+  submitColour.addEventListener('click', changeBackground);
+};
 
 main();
