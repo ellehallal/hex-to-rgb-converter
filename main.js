a05e901a7b49180dfc3d1ce499d80f784a573514
Elle H <itsellej@gmail.com>
2018-08-20 10:56:30 +0100

    feature: Add hextorgb function

--- main.js
+++ main.js
@@ -2,13 +2,27 @@ const hexInput = document.querySelector('input[type="text"]');
 const submitColour = document.getElementById('buttoncolour');
 
 
+
 const main = () => {
   const changeBackground = () => {
-    const hexValue = hexInput.value;
+    let hexValue = hexInput.value;
     document.body.style.backgroundColor = hexValue;
   };
 
+
+  const hextorgb = () => {
+    let hexInputValue = hexInput.value;
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexInputValue);
+    console.log(result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    } : null);
+
+  };
+
   submitColour.addEventListener('click', changeBackground);
+  hextorgb();
 };
 
 main();
