058e260afac6b0497b90c7cb66343d10ae746432
Elle H <itsellej@gmail.com>
2018-09-09 11:12:32 +0100

    Remove main function. Change border radius of container to 10px

--- main.js
+++ main.js
@@ -5,31 +5,28 @@ const resultr = document.querySelector('#resultr');
 const resultg = document.querySelector('#resultg');
 const resultb = document.querySelector('#resultb');
 
-const main = () => {
-  const changeBackground = () => {
-    let hexValue = hexInput.value;
-    document.body.style.backgroundColor = hexValue;
-  };
 
-
-  const hextorgb = () => {
-    let hexInputValue = hexInput.value;
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexInputValue);
-
-    resultr.innerHTML = `${parseInt(result[1], 16)}`;
-    resultg.innerHTML = `${parseInt(result[2], 16)}`;
-    resultb.innerHTML = `${parseInt(result[3], 16)}`;
-
-    // console.log(result ? {
-    //   r: parseInt(result[1], 16),
-    //   g: parseInt(result[2], 16),
-    //   b: parseInt(result[3], 16),
-    // } : null);
-
-  };
-
-  submitColour.addEventListener('click', changeBackground);
-  submitColour.addEventListener('click', hextorgb);
+const changeBackground = () => {
+  let hexValue = hexInput.value;
+  document.body.style.backgroundColor = hexValue;
 };
 
-main();
+
+const hextorgb = () => {
+  let hexInputValue = hexInput.value;
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexInputValue);
+
+  resultr.innerHTML = `${parseInt(result[1], 16)}`;
+  resultg.innerHTML = `${parseInt(result[2], 16)}`;
+  resultb.innerHTML = `${parseInt(result[3], 16)}`;
+
+  // console.log(result ? {
+  //   r: parseInt(result[1], 16),
+  //   g: parseInt(result[2], 16),
+  //   b: parseInt(result[3], 16),
+  // } : null);
+
+};
+
+submitColour.addEventListener('click', changeBackground);
+submitColour.addEventListener('click', hextorgb);
